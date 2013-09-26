6f8530c8c83eeda9b771886717334e46678eabde
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-09-26 15:09:11 +0200

    Add root 'pubblico#index'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Microfilmdigit::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pubblico#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
