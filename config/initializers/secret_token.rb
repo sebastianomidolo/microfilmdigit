64b0c8ce8867cdd6814d3f8e553bd9aff9d0082d
Sebastiano Midolo <sebastiano.mdl@gmail.com>
2013-09-26 12:56:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Microfilmdigit::Application.config.secret_key_base = '856b868428859ed217547187157964d118d096167ace97e360eac065ce2f6dc1bccbcae37e15e5dae2e040b331efd6f599fa9e22af190292476fa9cc62c80543'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Microfilmdigit::Application.config.session_store :cookie_store, key: '_microfilmdigit_session'
